136943f866e7696d7b475dfe5ac65166a350f286
Karl Bonde Torp <k.torp@samsung.com>
2023-01-27 13:35:42 +0100

    add container with devtools
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- gen-dockerfiles.py
+++ gen-dockerfiles.py
@@ -14,13 +14,17 @@ def main():
         raw = yaml.safe_load(yfile)
 
     with Path("dockerfile.template").open() as tfile:
-        template = Template(tfile.read())
+        default_template = Template(tfile.read())
+
+    with Path("citools.template").open() as tfile:
+        citools_template = Template(tfile.read())
 
     distros = raw["jobs"]["dockerize"]["strategy"]["matrix"]["container"]
     for distro in distros:
         path = Path("dockerfiles") / distro["os"] / distro["ver"] / "Dockerfile"
         path.parent.mkdir(parents=True, exist_ok=True)
         with path.open("w") as dfile:
+            template = default_template if distro["os"] != "citools" else citools_template
             dfile.write(template.render(distro=distro))
 
 if __name__ == "__main__":
